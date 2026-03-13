18062ea5c80ba04fa26db9dfabf0ef509ebfc745
rocio4679 <rocio4679@hotmail.com>
2026-03-13 19:23:02 -0400

    Control Flow commit

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -44,3 +44,9 @@ switch (userType) {
 }
 
 console.log("User Category: ", userCategory);
+
+let isAuthenticated = true;
+
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not Authenticated";
+
+console.log("Authentication Status: ", authenticationStatus);
