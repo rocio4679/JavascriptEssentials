2e154e3bce1cd5ed1c3e9a6402ca600816ab3084
rocio4679 <rocio4679@hotmail.com>
2026-03-15 20:53:24 -0400

    Add Reset button

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -6,8 +6,17 @@ function increaseCount() {
     checkCountValue(); //Checks followers
 }
 
+function resetCount() {
+    const resetCount = 0; 
+    count = 0; 
+    document.getElementById('countDisplay').innerHTML = resetCount; 
+    checkCountValue(); 
+}
+
 function checkCountValue() {
-    if(count === 10) {
+    if(count === 0) {
+        alert("Your Instagram post count has been reset!");
+    } else if (count === 10) {
         alert("Your Instagram post gained 10 followers! Congratulations!");
     } else if (count === 20) {
         alert("Your Instagram post gained 20 followers! Keep it up!");
