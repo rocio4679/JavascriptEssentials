b323baa3e1f8982b7af42012735c5d6751c4e70f
rocio4679 <rocio4679@hotmail.com>
2026-03-26 20:57:04 -0400

    Including speed writing test

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -33,6 +33,7 @@ function endTest() {
     }).length;
 
     var wpm = 0; //Default value
+    var totalLength = userTypedText.length; 
 
     if (timeElapsed !== 0 && !isNaN(typedWords)) {
         wpm = Math.round((typedWords / timeElapsed) * 60);
@@ -41,6 +42,7 @@ function endTest() {
     //Display the results
     var outputDiv = document.getElementById("output");
     outputDiv.innerHTML = "<h2>Typing Test Results: </h2>" +
+        "<p>Total Length: " + totalLength + "</p>" +
         "<p>Words Typed: " + typedWords + "</p>" +
         "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
         "<p>Words Per Minute (WPM): " + wpm + "</p>"; 
