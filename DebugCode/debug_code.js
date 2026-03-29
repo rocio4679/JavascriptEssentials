74c8a54e70179ddd5010963248b77774ef746282
rocio4679 <rocio4679@hotmail.com>
2026-03-28 21:24:44 -0400

    Debug exercise

---
+++ DebugCode/debug_code.js
@@ -0,0 +1,48 @@
+function performOperation() {
+    //Get user input from input fields
+    let num1 = parseInt(document.getElementById('input1').value); 
+    let num2 = parseInt(document.getElementById('input2').value);
+    //Check if inputs are valid numbers
+    if (!isNaN(num1) && !isNaN(num2)) {
+        //Perform the operation
+        let result = add(num1, num2); 
+        result = multiply(num1, num2);
+        result = divide(num1, num2);
+
+
+        //Display the result
+        displayResult(result); 
+    } else {
+        displayResult('Please enter valid numbers'); 
+    }
+}
+
+function add(a, b) {
+    //Introduce a debugger statement to pause execution
+    debugger; 
+    
+    //Add the numbers
+    return a + b; 
+}
+
+function multiply(a, b) {
+    //Introduce a debugger statement to pause execution
+    debugger; 
+
+    //Multiply the numbers
+    return a * b;
+}
+
+function divide(a, b) {
+    //Introduce a debugger statement to pause execution
+    debugger;
+
+    //Divide
+    return a / b; 
+}
+
+function displayResult(result) {
+    //Display the result in the paragraph element 
+    const resultElement = document.getElementById('result');
+    resultElement.textContent = `The result is: ${result}`;
+}
